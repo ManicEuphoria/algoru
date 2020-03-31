dc5e5a2a275389ad50178046bda163a7d8222ee7
taotsi <tmkf.guthrie@gmail.com>
2020-04-01 02:30:58 +0800

    A star path finding, with bug

--- lib/a_star.hh
+++ lib/a_star.hh
@@ -2,8 +2,10 @@
 #include <vector>
 #include <iostream>
 #include <cmath>
+#include <memory>
+#include <algorithm>
 
-using point_t = std::array<int, 2>;
+using vec2_t = std::array<int, 2>;
 
 class Map
 {
@@ -22,17 +24,18 @@ public:
     }
   }
 
-  void add_obstacle(point_t p)
+  void add_obstacle(vec2_t p)
   {
-    if(p[0] < w_max_ && p[1] < h_max_)
+    if(p[0] >= 0 && p[0] < w_max_ && p[1] >= 0 && p[1] < h_max_)
     {
       grids_[p[0]][p[1]] = OBSTACLE_CHAR;
     }
   }
 
-  void add_obstacle_wall(point_t p1, point_t p2)
+  void add_obstacle_wall(vec2_t p1, vec2_t p2)
   {
-    if(p1[0] > w_max_ || p2[0] > w_max_ || p1[1] > h_max_ || p2[1] > h_max_)
+    if(p1[0] > w_max_ || p2[0] > w_max_ || p1[1] > h_max_ || p2[1] > h_max_
+       || p1[0] < 0 || p1[1] < 0 || p2[0] < 0 || p2[1] < 0)
     {
       return;
     }
@@ -42,7 +45,7 @@ public:
 
     if(p1[0] == p2[0])
     {
-      for (int i = 0; i < y_max - y_min; i++)
+      for (int i = 0; i <= y_max - y_min; i++)
       {
         grids_[p1[0]][i + y_min] = OBSTACLE_CHAR;
       }
@@ -54,7 +57,7 @@ public:
 
     if(p1[1] == p2[1])
     {
-      for (int i = 0; i < x_max - x_min; i++)
+      for (int i = 0; i <= x_max - x_min; i++)
       {
         grids_[i + x_min][p1[1]] = OBSTACLE_CHAR;
       }
@@ -89,6 +92,55 @@ public:
       }
     }
 
+  }
+
+  std::vector<vec2_t> get_neighbors(vec2_t p) const
+  {
+    std::vector<vec2_t> neighbors;
+    if(p[0] > 0)
+    {
+      vec2_t p_neighbor{p[0] - 1, p[1]};
+      if(grids_[p_neighbor[0]][p_neighbor[1]] != OBSTACLE_CHAR)
+      {
+        neighbors.push_back(p_neighbor);
+      }
+    }
+    if(p[0] < w_max_ - 1)
+    {
+      vec2_t p_neighbor{p[0] + 1, p[1]};
+      if(grids_[p_neighbor[0]][p_neighbor[1]] != OBSTACLE_CHAR)
+      {
+        neighbors.push_back(p_neighbor);
+      }
+    }
+    if(p[1] > 0)
+    {
+      vec2_t p_neighbor{p[0], p[1] - 1};
+      if(grids_[p_neighbor[0]][p_neighbor[1]] != OBSTACLE_CHAR)
+      {
+        neighbors.push_back(p_neighbor);
+      }
+    }
+    if(p[1] < h_max_ - 1)
+    {
+      vec2_t p_neighbor{p[0], p[1] + 1};
+      if(grids_[p_neighbor[0]][p_neighbor[1]] != OBSTACLE_CHAR)
+      {
+        neighbors.push_back(p_neighbor);
+      }
+    }
+    return neighbors;
+  }
+
+  void add_path(const std::vector<vec2_t> &path)
+  {
+    for(const auto &p : path)
+    {
+      if(p[0] >= 0 && p[0] < w_max_ && p[1] >= 0 && p[1] < h_max_)
+      {
+        grids_[p[0]][p[1]] = PATH_CHAR;
+      }
+    }
   }
 
   void print() const
@@ -131,7 +183,128 @@ private:
   static constexpr char PATH_CHAR = 'o';
 };
 
-std::vector<point_t> a_star(const Map &map, point_t start, point_t target)
+inline double manhattan_distance(vec2_t a, vec2_t b)
 {
-  return {};
+  return std::abs(a[0] - b[0]) + std::abs(a[1] - b[1]);
 }
+
+class State
+{
+public:
+  State(vec2_t start, vec2_t target)
+    : target_{target}
+  {
+    path_.push_back(start);
+  }
+
+  std::vector<State> expand(std::shared_ptr<Map> map)
+  {
+    std::vector<State> result;
+    auto neighbor_grids = find_neighbor_grids(map);
+    for(auto grid : neighbor_grids)
+    {
+      auto temp = *this;
+      temp.path_.push_back(grid);
+      temp.path_length_ += 1;
+      result.push_back(std::move(temp));
+    }
+    return result;
+  }
+
+  double score() const
+  {
+    return path_length_ + manhattan_distance(path_.back(), target_);
+  }
+  bool is_terminal() const
+  {
+    return path_.back() == target_;
+  }
+  const std::vector<vec2_t>& path() const
+  {
+    return path_;
+  }
+private:
+  std::vector<vec2_t> find_neighbor_grids(std::shared_ptr<Map> map) const
+  {
+    auto potential_neighbors = map->get_neighbors(path_.back());
+    std::vector<vec2_t> neighbors;
+    for(auto nb : potential_neighbors)
+    {
+      bool been_there = false;
+      for(auto it = path_.rbegin(); it != path_.rend(); it++)
+      {
+        if((*it) == nb)
+        {
+          been_there = true;
+        }
+      }
+      if(!been_there)
+      {
+        neighbors.push_back(nb);
+      }
+    }
+    return neighbors;
+  }
+  std::vector<vec2_t> path_;
+  vec2_t target_;
+  double path_length_;
+};
+
+class AstarPath
+{
+public:
+  AstarPath(std::shared_ptr<Map> map, vec2_t start, vec2_t target)
+    : map_{map}, start_{start}, target_{target}
+  {
+    frontier_states_.emplace_back(start, target);
+    a_star_search();
+  }
+  const std::vector<vec2_t>& path() const
+  {
+    return best_path_;
+  }
+
+private:
+  void a_star_search()
+  {
+    int c = 0;
+    while(!frontier_states_.empty())
+    {
+      auto current_state = pop_most_promising_state();
+      std::cout << c++ << ", " << current_state.score() << "\r";
+      // auto temp_map = *map_;
+      // temp_map.add_path(current_state.path());
+      // temp_map.print();
+      if(current_state.is_terminal())
+      {
+        best_path_ = current_state.path();
+        break;
+      }
+      auto new_states = current_state.expand(map_);
+      frontier_states_.insert(
+        frontier_states_.end(), new_states.begin(), new_states.end());
+    }
+    std::cout << "\n";
+
+    if(best_path_.empty())
+    {
+      std::cout << "didn't find any path!\n";
+    }
+  }
+
+  State pop_most_promising_state()
+  {
+    std::sort(frontier_states_.begin(), frontier_states_.end(),
+              [](const auto &s1, const auto &s2){
+                return s1.score() > s2.score();});
+    auto result = frontier_states_.back();
+    frontier_states_.pop_back();
+    return result;
+  }
+
+  std::shared_ptr<Map> map_;
+  vec2_t start_;
+  vec2_t target_;
+  std::vector<State> frontier_states_;
+  std::vector<vec2_t> best_path_;
+};
--- lib/main.cc
+++ lib/main.cc
@@ -1,17 +1,12 @@
+#include <memory>
 #include "a_star.hh"
 
 int main()
 {
-  Map map{50, 20};
-  map.add_obstacle_wall({15, 2}, {35, 2});
-  map.add_obstacle_wall({15, 2}, {16, 6});
-  map.add_obstacle_wall({35, 2}, {34, 6});
-  map.add_obstacle_wall({16, 7}, {34, 7});
-  map.add_obstacle_wall({14, 11}, {37, 11});
-  map.add_obstacle_wall({14, 12}, {15, 15});
-  map.add_obstacle_wall({36, 12}, {35, 15});
-  map.add_obstacle_wall({15, 16}, {36, 16});
-
-  map.print();
+  auto map = std::make_shared<Map>(50, 20);
+  map->add_obstacle_wall({10, 4}, {13, 11});
+  AstarPath a_star_path{map, {3, 10}, {20, 10}};
+  map->add_path(a_star_path.path());
+  map->print();
   return 0;
 }
